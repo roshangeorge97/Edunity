c1b3df540b5068f19c6987d48d1620464320c481
Roshan George <93831830+roshangeorge97@users.noreply.github.com>
2023-01-31 13:10:36 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// )
+
+import reportWebVitals from "./reportWebVitals";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-)
+);
+
+reportWebVitals();
