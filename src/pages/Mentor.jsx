1dd6a59cb92640a4a6f84acb714788f6b124183a
Geeta <geetkiroula1999@gmail.com>
2023-06-16 15:55:15 +0530

    Mentor form UI improved by alligned all fields issue#36 resolved

--- src/pages/Mentor.jsx
+++ src/pages/Mentor.jsx
@@ -81,149 +81,163 @@ await setDoc(doc(db, "mentors", email), {
       <div className="mt-10 sm:mt-0 bg-yellow">
         <div className="md:grid md:grid-cols-3 md:gap-6 bg-yellow">
          
-          <div className="mt-5 md:col-span-2 md:mt-0">
-            <form onSubmit={handleSubmit} method="POST">
-              <div className="overflow-hidden shadow sm:rounded-md">
-                <div className="bg-white px-4 py-5 sm:p-6">
-                  
-                  <div className="grid grid-cols-6 gap-6">
+        <div className="mt-5 md:col-span-2 md:mt-0  flex flex-col justify-center">
 
-                  <div>
-            <input type="file" onChange={handleChange} accept="/image/*" />
-            <button onClick={handleUpload}>Upload to Firebase</button>
-            <p>{percent} "% done"</p>
+<form onSubmit={handleSubmit} method="POST" className='w-full flex flex-col py-6 gap-6'>
+
+  <div className="overflow-hidden shadow sm:rounded-md shadow-gray-600">
+
+    <div className="bg-white px-3 py-4">
+
+         <div className="flex flex-col w-full gap-2">
+        <label class="block text-sm font-medium text-gray-700 dark:text-white">Upload File</label>
+        <input onChange={handleChange} accept="/image/*"  className="block w-full text-lg text-gray-700 rounded-lg cursor-pointer" type="file"
+       
+        />
+
+        <div className="flex gap-5 mt-3"> 
+                 <button className='rounded-lg border-[1px] border-yellow w-30 text-black text-m px-2 active:outline-none h-10 cursor-pointer hover:bg-yellow hover:bg-opacity-57 hover:text-white' onClick={handleUpload}>Upload to Firebase</button>
+                <p className="text-center justify-center my-2">{percent} "% done"</p>
+            </div>
+
         </div>
-                    
-                    <div className="col-span-6 sm:col-span-3">
-                      <label htmlFor="first-name" className="block text-sm font-medium text-gray-700">
-                        First name
-                      </label>
-                      <input
-                        type="text"
-                        name="first-name"
-                        id="first-name"
-                        autoComplete="given-name"
-                        className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                        value={firstName}
-                        onChange={(e)=>setfirstName(e.target.value)}
-                      />
-                    </div>
+        
+        
 
-                    <div className="col-span-6 sm:col-span-3">
-                      <label htmlFor="last-name" className="block text-sm font-medium text-gray-700">
-                        Last name
-                      </label>
-                      <input
-                        type="text"
-                        name="last-name"
-                        id="last-name"
-                        autoComplete="family-name"
-                        className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                        value={lastName}
-                        onChange={(e)=>setlastName(e.target.value)}
-                      />
-                    </div>
+        <div className="md:flex w-full gap-5 my-5">
+            <div className="flex flex-col md:w-[50%] gap-2">
+              <label htmlFor="first-name" className="text-sm font-medium text-gray-700">
+                First name
+              </label>
+              <input
+                type="text"
+                name="first-name"
+                id="first-name"
+                autoComplete="given-name"
+                className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-yellow focus:ring-yellow sm:text-sm"
+                value={firstName}
+                onChange={(e)=>setfirstName(e.target.value)}
+              />
+            </div>
 
-                    <div className="col-span-6 sm:col-span-4">
-                      <label htmlFor="email-address" className="block text-sm font-medium text-gray-700">
-                        Email address
-                      </label>
-                      <input
-                        type="text"
-                        name="email-address"
-                        id="email-address"
-                        autoComplete="email"
-                        className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                        value={email}
-                        onChange={(e)=>setEmail(e.target.value)}
-                      />
-                    </div>
-
-                    <div className="col-span-6 sm:col-span-3">
-                      <label htmlFor="country" className="block text-sm font-medium text-gray-700">
-                        PhoneNumber
-                      </label>
-                      <input
-                        type="text"
-                        name="email-address"
-                        id="email-address"
-                        autoComplete="email"
-                        className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                        value={phone}
-                        onChange={(e)=>setPhone(e.target.value)}
-                      />
-                    </div>
-
-                    <div className="col-span-6">
-                      <label htmlFor="street-address" className="block text-sm font-medium text-gray-700">
-                        Street address
-                      </label>
-                      <input
-                        type="text"
-                        name="street-address"
-                        id="street-address"
-                        autoComplete="street-address"
-                        className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                        value={address}
-                        onChange={(e)=>setAddress(e.target.value)}
-                      />
-                    </div>
-                    <div className="col-span-6">
-                      <label htmlFor="street-address" className="block text-sm font-medium text-gray-700">
-                        Subject
-                      </label>
-                      <input
-                        type="text"
-                        name="street-address"
-                        id="street-address"
-                        autoComplete="street-address"
-                        className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                        value={subject}
-                        onChange={(e)=>setSubject(e.target.value)}
-                      />
-                    </div>
-                    <div className="col-span-6">
-                      <label htmlFor="street-address" className="block text-sm font-medium text-gray-700">
-                        Language
-                      </label>
-                      <input
-                        type="text"
-                        name="street-address"
-                        id="street-address"
-                        autoComplete="street-address"
-                        className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                        value={language}
-                        onChange={(e)=>setLanguage(e.target.value)}
-                      />
-                    </div>
+        <div className="flex flex-col md:w-[50%] mt-3 md:mt-0 gap-2">
+          <label htmlFor="last-name" className="text-sm font-medium text-gray-700">
+            Last name
+          </label>
+          <input
+            type="text"
+            name="last-name"
+            id="last-name"
+            autoComplete="family-name"
+            className="mt-1 block w-full rounded-md border-gray-300 shadow-sm  focus:border-yellow focus:ring-yellow sm:text-sm"
+            value={lastName}
+            onChange={(e)=>setlastName(e.target.value)}
+          />
+        </div>
 
 
-                    <div className="col-span-6">
-                      <label htmlFor="street-address" className="block text-sm font-medium text-gray-700">
-                        Resume
-                      </label>
-                      <input
-                        type="file"
-                        name="street-address"
-                        id="street-address"
-                        autoComplete="street-address"
-                        className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                      />
-                    </div>
+        </div>
+      
+        <div className="flex flex-col gap-2 mt-5">
+          <label htmlFor="email-address" className="block text-sm font-medium text-gray-700">
+            Email address
+          </label>
+          <input
+            type="text"
+            name="email-address"
+            id="email-address"
+            autoComplete="email"
+            className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-yellow focus:ring-yellow sm:text-sm"
+            value={email}
+            onChange={(e)=>setEmail(e.target.value)}
+          />
+        </div>
 
-                  </div>
-                </div>
-                <div className="bg-gray-50 px-4 py-3 text-right sm:px-6">
-                  <button
-                    type="submit"
-                    className="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                  >
-                    Save
-                  </button>
-                </div>
-              </div>
-            </form>
-          </div>
+        <div className="flex flex-col gap-2 mt-5">
+          <label htmlFor="country" className="block text-sm font-medium text-gray-700">
+            PhoneNumber
+          </label>
+          <input
+            type="text"
+            name="email-address"
+            id="email-address"
+            autoComplete="email"
+            className="mt-1 block w-full rounded-md border-gray-300 shadow-sm  focus:border-yellow focus:ring-yellow sm:text-sm"
+            value={phone}
+            onChange={(e)=>setPhone(e.target.value)}
+          />
+        </div>
+
+        <div className="flex flex-col gap-2 mt-5">
+          <label htmlFor="street-address" className="block text-sm font-medium text-gray-700">
+            Street address
+          </label>
+          <input
+            type="text"
+            name="street-address"
+            id="street-address"
+            autoComplete="street-address"
+            className="mt-1 block w-full rounded-md border-gray-300 shadow-sm  focus:border-yellow focus:ring-yellow sm:text-sm"
+            value={address}
+            onChange={(e)=>setAddress(e.target.value)}
+          />
+        </div>
+        <div className="flex flex-col gap-2 mt-5">
+          <label htmlFor="street-address" className="block text-sm font-medium text-gray-700">
+            Subject
+          </label>
+          <input
+            type="text"
+            name="street-address"
+            id="street-address"
+            autoComplete="street-address"
+            className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-yellow focus:ring-yellow sm:text-sm"
+            value={subject}
+            onChange={(e)=>setSubject(e.target.value)}
+          />
+        </div>
+        <div className="flex flex-col gap-2 mt-5">
+          <label htmlFor="street-address" className="block text-sm font-medium text-gray-700">
+            Language
+          </label>
+          <input
+            type="text"
+            name="street-address"
+            id="street-address"
+            autoComplete="street-address"
+            className="mt-1 block w-full rounded-md border-gray-300 shadow-sm  focus:border-yellow focus:ring-yellow sm:text-sm"
+            value={language}
+            onChange={(e)=>setLanguage(e.target.value)}
+          />
+        </div>
+
+
+        <div className="flex flex-col gap-2 mt-5">
+          <label htmlFor="street-address" className="block text-sm font-medium text-gray-700 dark:text-white">
+            Resume
+          </label>
+          <input
+            type="file"
+            name="street-address"
+            id="street-address"
+            autoComplete="street-address"
+            className="mt-1 block w-full text-lg rounded-md border-gray-300 shadow-sm  focus:border-yellow focus:ring-yellow sm:text-sm"
+          />
+        </div>
+
+    </div>
+    <div className="bg-gray-50 px-4 py-3 text-right sm:px-6">
+      <button
+        type="submit"
+        className="inline-flex justify-center rounded-md py-2 px-4 text-m font-medium focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2
+        rounded-lg border border-white bg-yellow text-white w-30 text-black text-m px-2 active:outline-none h-10 cursor-pointer hover:bg-green hover:text-yellow hover:border-green"
+      >
+        Submit
+      </button>
+    </div>
+  </div>
+</form>
+</div>
         </div>
       </div>
 
